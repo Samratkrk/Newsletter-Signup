d48dfb17b36ceb1a74143efa7d5670f1db43a2a7
Samrat Karki <samratkrk@outlook.com>
2022-08-02 16:35:04 -0400

    Setting up the signin page

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstname = req.body.fname;
+  const lastname = req.body.lname;
+  const email = req.body.email;
+  console.log(firstname, lastname, email);
+});
+
+app.listen(3000, function () {
+  console.log("Localhost Running on Port 3000");
+});
