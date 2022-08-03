429dc0f54480c54c755a4bb75bca76a6446e166b
Samrat Karki <samratkrk@outlook.com>
2022-08-03 10:30:54 -0400

    Bootstrapping Success and Failure Page

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ app.post("/", function (req, res) {
   };
 
   const jsonData = JSON.stringify(data);
-  const url = "https://us13.api.mailchimp.com/3.0/lists/"+ process.env.AUDIENCE_ID + "";
+  const url =
+    "https://us13.api.mailchimp.com/3.0/lists/" + process.env.AUDIENCE_ID + "";
   const option = {
     method: "POST",
     auth: "Samscript55:" + process.env.API_KEY,
@@ -42,9 +43,9 @@ app.post("/", function (req, res) {
 
   const request = https.request(url, option, function (response) {
     if (response.statusCode === 200) {
-      res.send("Successfully Subscribed!");
+      res.sendFile(__dirname + "/success.html");
     } else {
-      res.send("There was an error with signing up, please try again!");
+      res.sendFile(__dirname + "/failure.html");
     }
 
     response.on("data", function (data) {
@@ -54,6 +55,10 @@ app.post("/", function (req, res) {
 
   request.write(jsonData);
   request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, function () {
