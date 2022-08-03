c19be92efb44a9ec7d8e5e5941967781198a3643
Samrat Karki <samratkrk@outlook.com>
2022-08-03 10:07:46 -0400

    Posting Data to Mailchimps Servers via their API

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -16,9 +19,43 @@ app.post("/", function (req, res) {
   const firstname = req.body.fname;
   const lastname = req.body.lname;
   const email = req.body.email;
-  console.log(firstname, lastname, email);
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstname,
+          LNAME: lastname,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us13.api.mailchimp.com/3.0/lists/"+ process.env.AUDIENCE_ID + "";
+  const option = {
+    method: "POST",
+    auth: "Samscript55:" + process.env.API_KEY,
+  };
+
+  const request = https.request(url, option, function (response) {
+    if (response.statusCode === 200) {
+      res.send("Successfully Subscribed!");
+    } else {
+      res.send("There was an error with signing up, please try again!");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
-app.listen(3000, function () {
-  console.log("Localhost Running on Port 3000");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Localhost Running on Port 3000 successfully");
 });
